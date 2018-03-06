991df841605f00af977c91a6b39d8747250b95b0
NayakArnimesh2015 <36810026+NayakArnimesh2015@users.noreply.github.com>
2018-03-06 22:15:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,22 @@
-console.log('Loaded!');
-//changes are to be made
+var button = document.getElementById('counter');
 
-var element = document.getElementById('main-text');
-element.innerHTML='NEW VALUE';
-
-//move the image
- var img = document.getElementById('madi');
- var marginLeft = 0;
- function moveRight() {
-     marginLeft = marginLeft + 10;
-     img.style.marginLeft = marginleft + 'px';
- }
- img.onclick = function() {
-     var interval = setInterval(moveRight ,100);
-     
- };
+button.onclick = function () {
+    
+    //create a request object
+    var request = new XMLhttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //TAKE ACTION
+            if(request.status===200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    //make the request
+    request.open('GET','http://http://nayakarnimesh2015.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
