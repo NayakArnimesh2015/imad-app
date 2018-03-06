6b859f48ab8d262cbcdfb55d6c3c728306b9b09a
NayakArnimesh2015 <36810026+NayakArnimesh2015@users.noreply.github.com>
2018-03-06 07:11:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ element.innerHTML='NEW VALUE';
 
 //move the image
  var img = document.getElementById('madi');
- img.onclick =function() {
-     img.style.marginleft = '100px' ;
+ img.onclick = function() {
+     img.style.marginLeft = '100px';
  };
